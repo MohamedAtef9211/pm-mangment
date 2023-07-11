7497c14cc86f5c3dff21f16122007c35906be7c9
Mohamed Atef <100563509+MohamedAtef9211@users.noreply.github.com>
2023-07-11 15:04:30 +0300

    Updating FeatureController

--- application-commons/src/main/java/com/base/pm/common/client/feature/FeatureController.java
+++ application-commons/src/main/java/com/base/pm/common/client/feature/FeatureController.java
@@ -3,7 +3,7 @@
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
- @RestController
+@RestController
 @RequestMapping("/feature-togglz")
 @RequiredArgsConstructor
 public class FeatureController {
